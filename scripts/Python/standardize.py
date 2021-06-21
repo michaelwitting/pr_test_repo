af7b42b892cdbd41043b95b15bdd1245fcab0e48
Fleming Kretschmer <fleming.kretschmer@uni-jena.de>
2021-06-21 12:21:46 +0200

    debug python standardize

--- scripts/Python/standardize.py
+++ scripts/Python/standardize.py
@@ -5,12 +5,11 @@ import sys
 with open(sys.argv[1]) as inhandle, open(sys.argv[1] + '_standardized', 'w') as success_handle, open(sys.argv[1] + '_failed', 'w') as fail_handle:
     for line in inhandle.readlines():
         id_, smiles = line.split('\t')
+        smiles = smiles.strip()
+        if (len(smiles) == 0):
+            continue
         try:
             c = pubchempy.get_compounds(smiles, 'smiles')[0]
-            succ_out = '\t'.join([id_, c.isomeric_smiles]) + '\n'
-            print('succ_out: ' + succ_out, end='')
-            success_handle.write(succ_out)
+            success_handle.write('\t'.join([id_, c.isomeric_smiles]) + '\n')
         except Exception as e:
-            fail_out = '\t'.join([id_, smiles, str(e)]) + '\n'
-            print('fail_out: ' + fail_out, end='')
-            fail_handle.write(fail_out)
+            fail_handle.write('\t'.join([id_, smiles, str(e).strip()]) + '\n')
