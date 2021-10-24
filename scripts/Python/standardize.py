0a713d25d79c6df8efd0a6bb1111a5d1c3e48b39
Fleming Kretschmer <fleming.kretschmer@uni-jena.de>
2021-10-24 15:29:39 +0200

    remove debug edits

--- scripts/Python/standardize.py
+++ scripts/Python/standardize.py
@@ -1,16 +1,8 @@
 # coding: utf-8
 import pubchempy
 import sys
-import os
-
-print(sys.version)
-print(sys.argv[1] + '_standardized')
-print(sys.argv[1] + '_failed')
-print(os.getcwd())
-print('writable?', os.access(os.getcwd(), os.W_OK))
 
 with open(sys.argv[1]) as inhandle, open(sys.argv[1] + '_standardized', 'w') as success_handle, open(sys.argv[1] + '_failed', 'w') as fail_handle:
-    print('opening files')
     for line in inhandle.readlines():
         id_, smiles = line.split('\t')
         smiles = smiles.strip()
@@ -24,4 +16,3 @@ with open(sys.argv[1]) as inhandle, open(sys.argv[1] + '_standardized', 'w') as 
     # make sure output files (even if empty) exist
     success_handle.write('')
     fail_handle.write('')
-    print('finished, trying to close')
