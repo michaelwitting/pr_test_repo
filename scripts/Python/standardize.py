46cb54ea0831b00ddee744d9388dc539d0bccfa5
Fleming Kretschmer <fleming.kretschmer@uni-jena.de>
2021-10-24 12:47:52 +0200

    debugging python OSError

--- scripts/Python/standardize.py
+++ scripts/Python/standardize.py
@@ -2,6 +2,10 @@
 import pubchempy
 import sys
 
+print(sys.version)
+print(sys.argv[1] + '_standardized')
+print(sys.argv[1] + '_failed')
+
 with open(sys.argv[1]) as inhandle, open(sys.argv[1] + '_standardized', 'w') as success_handle, open(sys.argv[1] + '_failed', 'w') as fail_handle:
     for line in inhandle.readlines():
         id_, smiles = line.split('\t')
